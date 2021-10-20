963ba3cf1f58e0a9907cbd762c33de5f43c3248b
julim <minuk926@inpix.com>
2021-10-20 13:04:43 +0900

    typescript 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './app';
 import { Youtube } from './service/youtube';
 
+// .둪 : REACT_APP_YOUTUBE_API_KEY=AIzaSyCTpUx32TR3DuU3UQZn6GjYF7qCtMDfrj8
 const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY!);
 
 ReactDOM.render(
